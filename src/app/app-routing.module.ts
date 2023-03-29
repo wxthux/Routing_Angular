f48dfaac502cf641cc1d706991a7a9e9af3ccd34
Thanuda Warnakulage <thanudaw@zone24x7.lk>
2023-03-29 09:52:07 +0530

    Angular routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//import { user } from './user/user.component';
+//import { SecondComponent } from './second/second.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { LocationComponent } from './location/location.component';
+import { UserComponent } from './user/user.component';
+import {HttpClientModule} from '@angular/common/http';
 
+const appRoutes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: 'appointment', component: AppointmentComponent },
+  { path: 'location', component: LocationComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointmentComponent,
+    LocationComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   // AppRoutingModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
